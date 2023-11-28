255caa89bd9b1155ce9733bd7355d85477642fd1
Shem <111014629+shmcode@users.noreply.github.com>
2023-11-28 15:55:40 -0600

    Delete homepagereact directory

--- homepagereact/inicio/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { Inicio } from "./screens/Inicio";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<Inicio />);
--- homepagereact/sesion/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { Sesin } from "./screens/Sesin";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<Sesin />);
